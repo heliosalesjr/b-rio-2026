2aade8bd034d18f03c9f6b18ad658b5afd4903f1
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-10-30 12:56:08 -0300

    company title styles fixed

--- src/components/content/EF4.jsx
+++ src/components/content/EF4.jsx
@@ -40,10 +40,10 @@ const EF4 = () => {
       </div>
       <div className="px-6 py-8 max-w-5xl mx-auto">
         <h1 className="text-3xl font-bold mb-4 text-center text-slate-800">
-          Compreendendo o programa da BEĨ Educação e a Aprendizagem Baseada em Projetos
+          Compreendendo o programa da <span style={{ whiteSpace: 'nowrap' }} className='font-bold'>BEĨ Educação</span> e a Aprendizagem Baseada em Projetos
         </h1>
         <p className="text-lg leading-relaxed text-slate-700">
-          O Programa de Educação Financeira da BEĨ Educação foi elaborado com o objetivo de levar para a sala de aula a Educação Financeira, ancorada na disciplina de Matemática, por meio de um material acessível, que amparado em problemas reais do dia a dia, pretende despertar e mobilizar a atenção do estudante para lidar de forma responsável com seus recursos ao mesmo que apreende conceitos fundamentais da Matemática.
+          O Programa de Educação Financeira da <span style={{ whiteSpace: 'nowrap' }} className='font-bold'>BEĨ Educação</span> foi elaborado com o objetivo de levar para a sala de aula a Educação Financeira, ancorada na disciplina de Matemática, por meio de um material acessível, que amparado em problemas reais do dia a dia, pretende despertar e mobilizar a atenção do estudante para lidar de forma responsável com seus recursos ao mesmo que apreende conceitos fundamentais da Matemática.
         </p>
       </div>
     </div>
